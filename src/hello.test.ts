d720a4f646e69db6498f8349d7fe61af554973ec
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2024-07-12 18:04:52 +0200

    generalize I

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -21,8 +21,11 @@ describe("Roman numerals", () => {
 })
 
 const romanNumerals = (arabic: arabic): roman => {
-  if (arabic === 1) return "I"
-  if (arabic === 2) return "II"
-  if (arabic === 3) return "III"
+  if (arabic >= 0 && arabic < 4) {
+    let roman = ""
+    for (let i = 1; i <= arabic; i++) roman += "I"
+    return roman
+  }
+
   throw new Error("Unspecified behaviour")
 }
