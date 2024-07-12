e202355a018020fb5f683404c6be56bee8978d1e
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2024-07-12 18:00:52 +0200

    III

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -14,6 +14,7 @@ describe("Roman numerals", () => {
   it.each([
     { arabic: 1, roman: "I" },
     { arabic: 2, roman: "II" },
+    { arabic: 3, roman: "III" },
   ])("$arabic => $roman", ({ arabic, roman }) => {
     expect(romanNumerals(arabic)).toEqual(roman)
   })
@@ -22,5 +23,6 @@ describe("Roman numerals", () => {
 const romanNumerals = (arabic: arabic): roman => {
   if (arabic === 1) return "I"
   if (arabic === 2) return "II"
+  if (arabic === 3) return "III"
   throw new Error("Unspecified behaviour")
 }
