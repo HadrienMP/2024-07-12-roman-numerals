8236253bc9e9608bd153575ac11ea2b89e717381
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2024-07-12 18:17:57 +0200

    generalize and refactor

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -5,29 +5,34 @@ type arabic = number
 
 const outOfRangeNumber = -1
 describe("Roman numerals", () => {
-  it("throw an unexpected behaviour when it is not specified", () => {
-    expect(() => romanNumerals(outOfRangeNumber)).toThrow(
-      "Unspecified behaviour",
-    )
-  })
+  it.each([outOfRangeNumber, 4])(
+    "throw an unexpected behaviour when it is not specified: %i",
+    (arabic) => {
+      expect(() => romanNumerals(arabic)).toThrow("Unspecified behaviour")
+    },
+  )
 
   it.each([
     { arabic: 1, roman: "I" },
     { arabic: 2, roman: "II" },
     { arabic: 3, roman: "III" },
     { arabic: 5, roman: "V" },
+    { arabic: 6, roman: "VI" },
   ])("$arabic => $roman", ({ arabic, roman }) => {
     expect(romanNumerals(arabic)).toEqual(roman)
   })
 })
 
 const romanNumerals = (arabic: arabic): roman => {
-  if (arabic === 5) return "V"
-  if (arabic >= 0 && arabic < 4) {
-    let roman = ""
-    for (let i = 1; i <= arabic; i++) roman += "I"
-    return roman
+  if (arabic < 0 || arabic === 4 || arabic > 6)
+    throw new Error("Unspecified behaviour")
+
+  let roman = ""
+  if (arabic >= 5) {
+    roman = "V"
+    arabic -= 5
   }
 
-  throw new Error("Unspecified behaviour")
+  for (let i = 1; i <= arabic; i++) roman += "I"
+  return roman
 }
